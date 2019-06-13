351f5347053048672189ca3bcbf6f1082ead9ce6
Miguel Raggi <mraggi@gmail.com>
2019-06-13 18:17:40 -0500

    Added C7_diamante

---
+++ C7_diamante_en_cuadro.py
@@ -0,0 +1,19 @@
+import random
+
+def punto_en_cuadro():
+    return (random.random(), random.random())
+
+def esta_en_diamante(x,y):
+    return x+y < 1 and x-y < 1 and -x+y < 1 and -x-y < 1
+
+simulaciones = 100000
+
+dentro = 0
+for simulacion in range(simulaciones):
+    (x,y) = punto_en_cuadro()
+    if esta_en_diamante(x,y):
+        dentro += 1
+
+
+probabilidad = dentro/simulaciones
+print(f"P(caer dentro)={probabilidad}")
